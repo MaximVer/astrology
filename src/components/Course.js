e7da0494752f62d4296c91569281bc270a150966
Maxim Vershinin <mvershinin@chewy.com>
2018-07-13 15:48:17 -0400

    Completed implementation of connection to contentful api

--- src/components/Course.js
+++ src/components/Course.js
@@ -12,7 +12,7 @@ const Course = (props) => {
             { props.course ? (
                 <Card >
                     <CardMedia style={{height: 0, paddingTop: '56.25%'}}
-                               image={props.course.fields.courseImage.fields.file.url}
+                               image={props.course.fields.image.fields.file.url}
                                title={props.course.fields.title}
                     />
                     <CardContent>
