e6440a9a028feef145c12d2f073004e504440d88
GUILLERMO <theguilleh@gmail.com>
2018-07-25 16:31:54 -0400

    Added bg img to App
    Created a class name ‘bg-img’ and assigned it to the main div that holds NavBar, CoursesList and BlogsList.
    We may also create a class in App.css and assign it to main div, insted of using in-line .CSS.
    Both methods are correct.

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import CoursesList from './components/CoursesList'
 import './App.css';
 import BlogsList from "./components/BlogsList";
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      backgroundImage: "url('//images.ctfassets.net/pqvqz7pi9hj3/5JmCeRKUQEOQKi26GGiiCG/44efbf38149b55b9729392d1a93bd3cf/SljZ.gif')"
+    }
+  }
   render() {
     return (
-        <div>
+        <div className='bg-img' style={{backgroundImage: this.state.backgroundImage, width: 1360, height: 1360}}>
             <NavBar />
             <CoursesList />
             <BlogsList />
