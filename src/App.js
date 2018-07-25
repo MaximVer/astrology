9295ba22cd5d0bae105defd041bcc91ae949abc4
Maxim Vershinin <maxver234@gmail.com>
2018-07-25 17:32:14 -0400

    Revert "Added bg img to App"
    This reverts commit e6440a9a028feef145c12d2f073004e504440d88.

--- src/App.js
+++ src/App.js
@@ -4,16 +4,10 @@ import CoursesList from './components/CoursesList'
 import './App.css';
 import BlogsList from "./components/BlogsList";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      backgroundImage: "url('//images.ctfassets.net/pqvqz7pi9hj3/5JmCeRKUQEOQKi26GGiiCG/44efbf38149b55b9729392d1a93bd3cf/SljZ.gif')"
-    }
-  }
+class App extends Component {
   render() {
     return (
-        <div className='bg-img' style={{backgroundImage: this.state.backgroundImage, width: 1360, height: 1360}}>
+        <div>
             <NavBar />
             <CoursesList />
             <BlogsList />
